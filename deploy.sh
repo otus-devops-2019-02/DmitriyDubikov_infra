6a7bf4a7fe3b61c7cba3c1099dcfeddb9dc41a3d
DmitriyDubikov <ddi1@mail.ru>
2019-04-11 22:37:57 +0400

    homework 6

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
+
 cd /home/appuser
 echo ${PWD}
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
 ps aux | grep puma
+
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
+set -e
+
+# Install ruby
+apt update
+apt install -y ruby-full ruby-bundler build-essential
+
