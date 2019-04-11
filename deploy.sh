e0c6a5783003aadcd8b2d341ad372655a75cf8c0
DmitriyDubikov <ddi1@mail.ru>
2019-04-11 11:19:13 +0400

    homework 6

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+echo ${PWD}
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
 ps aux | grep puma
-echo "ddi test"
-echo ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-echo "ddi test2"
