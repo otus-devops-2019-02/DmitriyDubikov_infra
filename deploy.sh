fa0fa09038ef1a0a7d8e7c28ecc36a5e3f009806
DmitriyDubikov <ddi1@mail.ru>
2019-04-11 09:42:07 +0400

    homework 6

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo git clone -b monolith https://github.com/express42/reddit.git
-sudo cd reddit && bundle install
-sudo puma -d
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
 ps aux | grep puma
