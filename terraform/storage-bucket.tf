0f0826bba6c63cd48b73efbab5ecb64fc687d498
DmitriyDubikov <ddi1@mail.ru>
2019-04-23 22:59:49 +0400

    homework 9

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -6,8 +6,9 @@ region = "${var.region}"
 module "storage-bucket" {
 source = "SweetOps/storage-bucket/google"
 version = "0.1.1"
+
 # Имена поменяйте на другие
-name = ["storage-bucket-test", "storage-bucket-test2"]
+name = ["storage-bucket1", "storage-bucket2"]
 }
 output storage-bucket_url {
 value = "${module.storage-bucket.url}"
