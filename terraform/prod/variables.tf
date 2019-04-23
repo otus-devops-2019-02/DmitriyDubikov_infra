8c8e01c8ddec68cae53e93e81f91403ed77b2bd2
DmitriyDubikov <ddi1@mail.ru>
2019-04-23 22:03:25 +0400

    homework 9

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -29,3 +29,13 @@ variable zone {
   # Значение по умолчанию
   default = "europe-west1-d"
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
